5da85eb72b49a4005eac3111d4bad2bd7d401062
zChipan <hawari.urfan881@gmail.com>
2025-01-03 16:02:38 +0700

    Mengubah struktur table

--- gudang_diagram_erd 1.0.0.sql
+++ gudang_diagram_erd 1.0.0.sql
@@ -1,7 +1,6 @@
 -- Schema db design gudang sederhana ( masih tahap awal )
 -- db_gudang_prod (name db)
-
-
+-- version 1.0.0
 
 CREATE TABLE `gudang` (
   `id_gudang` int PRIMARY KEY AUTO_INCREMENT,
@@ -101,3 +100,74 @@ CREATE VIEW view_gudang AS
 SELECT
 gg.nama_gudang, kg.lebar_kapasitas_gudang, kg.tinggi_kapasitas_gudang, kg.muatan_kapasitas_gudang, kg.jumlah_rak_kapasitas_gudang, cg.nama_city
 FROM gudang gg JOIN kapasitas_gudang kg ON gg.id_kapasitas_gudang = kg.id_kapasitas_gudang JOIN city_gudang cg ON gg.id_city_gudang = cg.id_city_gudang
+
+-- Menampilkan informasi tentang rak gudang seperti nomor, kapasitas, dan status rak
+CREATE VIEW view_rak_gudang AS
+SELECT rg.nomor_rak_gudang, rg.lebar_kapasitas_rak_gudang, rg.tinggi_kapasitas_rak_gudang, rg.muatan_kapasitas_rak_gudang, rg.status_kapasitas_rak_gudang
+FROM rak_gudang rg
+
+-- alter tabel
+-- hapus muatan_kapasitas_gudang
+-- kemudian bikin banyak nya di table rak_gudang itu ada banyak juga di produk (many to many)
+
+-- hapus foregn_key dari tabel rak gudang dan produk
+ALTER TABLE `rak_gudang_produk`
+DROP FOREIGN KEY `rak_gudang_id_rak_gudang`;
+
+ALTER TABLE `rak_gudang_produk`
+DROP FOREIGN KEY `produk_id_rak_gudang`;
+
+
+CREATE TABLE produk_rak_gudang_mapping (
+  produk_id INT,
+  rak_gudang_id INT,
+  FOREIGN KEY (produk_id) REFERENCES produk(produk_id),
+  FOREIGN KEY (rak_gudang_id) REFERENCES rak_gudang(id_rak_gudang)
+);
+
+
+CREATE TABLE gudang_rak_gudang_mapping (
+  gudang_id INT,
+  rak_gudang_id INT,
+  FOREIGN KEY (gudang_id) REFERENCES gudang(id_gudang),
+  FOREIGN KEY (rak_gudang_id) REFERENCES rak_gudang(id_rak_gudang)
+);
+
+
+ALTER TABLE gudang
+DROP FOREIGN KEY id_city_gudang;
+
+SHOW CREATE TABLE gudang;
+
+
+CREATE TABLE `gudang` (  `id_gudang` int NOT NULL AUTO_INCREMENT,  `nama_gudang` varchar(255) NOT NULL,  `id_city_gudang` int DEFAULT NULL,  `id_kapasitas_gudang` int DEFAULT NULL,  `status` enum('created','running','done','failure') DEFAULT NULL,  `created_at` timestamp NULL DEFAULT (now()),  PRIMARY KEY (`id_gudang`),  UNIQUE KEY `nama_gudang` (`nama_gudang`),  KEY `id_city_gudang` (`id_city_gudang`),  KEY `id_kapasitas_gudang` (`id_kapasitas_gudang`),  KEY `idx_gudang` (`id_gudang`),  CONSTRAINT `gudang_ibfk_1` FOREIGN KEY (`id_city_gudang`) REFERENCES `city_gudang` (`id_city_gudang`),  CONSTRAINT `gudang_ibfk_2` FOREIGN KEY (`id_kapasitas_gudang`) REFERENCES `kapasitas_gudang` (`id_kapasitas_gudang`),  CONSTRAINT `gudang_ibfk_3` FOREIGN KEY (`id_city_gudang`) REFERENCES `city_gudang` (`id_city_gudang`),  CONSTRAINT `gudang_ibfk_4` FOREIGN KEY (`id_kapasitas_gudang`) REFERENCES `kapasitas_gudang` (`id_kapasitas_gudang`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='add constraint : nama_gudang'
+
+
+
+ALTER TABLE gudang
+DROP FOREIGN KEY gudang_ibfk_3;
+
+
+
+SHOW CREATE TABLE rak_gudang
+
+ALTER TABLE rak_gudang
+DROP FOREIGN KEY rak_gudang_ibfk_2;
+
+
+
+SHOW CREATE TABLE kapasitas_gudang
+
+
+CREATE TABLE `kapasitas_gudang` (  `id_kapasitas_gudang` int NOT NULL AUTO_INCREMENT,  `lebar_kapasitas_gudang` float DEFAULT '0',  `tinggi_kapasitas_gudang` float DEFAULT '0',  `muatan_kapasitas_gudang` float DEFAULT '0',  `jumlah_rak_kapasitas_gudang` int DEFAULT '0',  PRIMARY KEY (`id_kapasitas_gudang`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+SHOW CREATE TABLE produk
+
+ALTER TABLE produk
+DROP FOREIGN KEY produk_ibfk_2;
+
+
+
+ALTER TABLE gudang
+DROP FOREIGN KEY gudang_ibfk_4;
